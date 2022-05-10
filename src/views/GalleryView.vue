8bbfbc2c9a25ac1021a52bed277e2111ac7c4161
does1tmatter <laisvunasmsk@gmail.com>
2022-05-10 17:23:09 +0300

    sick

--- src/views/GalleryView.vue
+++ src/views/GalleryView.vue
@@ -21,13 +21,15 @@ useInfiniteScroll(el, () => {
 
 onMounted(() => {
     handleData()
+    console.log(data.value)
 })
 </script>
 
 <template>
     <div class="text-center mt-20">
-        <div v-for="(item, i) in data" :key="i">
-            {{ item.name }}
+        <div v-for="(item, i) in data" :key="i" class="mt-4">
+            {{ item.name }}<br>
+            <span v-for="(trait, i) in item.attributes" class="ml-4">{{ trait.value }}</span>
         </div>
     </div>
 </template>
