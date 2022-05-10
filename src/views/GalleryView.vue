25f5f3f967b86b5d89d6add36dd0d389ab2d62b5
does1tmatter <laisvunasmsk@gmail.com>
2022-05-10 19:09:40 +0300

    q

--- src/views/GalleryView.vue
+++ src/views/GalleryView.vue
@@ -27,16 +27,14 @@ onMounted(() => {
 <template>
     <div class="text-center mt-20">
         <div v-for="(item, i) in data" :key="i" class="mt-8">
-            <div>
+            <div class="uppercase">
                 <img :src="fixURL(item.image)" :alt="item.name" class="w-14 inline rounded-full">
                 {{ item.name }}
             </div>
-            <div class="flex gap-10 justify-center">
+            <div class="flex gap-10 justify-center text-[12px] mt-4">
                 <div v-for="(trait, i) in item.attributes" class="uppercase">
-                    <div>
-                        {{ trait.trait_type }}<br>
-                        {{ trait.value }}
-                    </div>
+                    {{ trait.trait_type }}<br>
+                    {{ trait.value }}
                 </div>
             </div>
         </div>
