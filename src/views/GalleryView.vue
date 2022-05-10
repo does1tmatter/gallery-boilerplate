5ccd242ec65e0d60f8d2d88e00f602cfc256dd2c
does1tmatter <laisvunasmsk@gmail.com>
2022-05-10 19:40:58 +0300

    cool

--- src/views/GalleryView.vue
+++ src/views/GalleryView.vue
@@ -6,6 +6,7 @@ import meta from '@/assets/meta.json'
 
 const { paginate, fixURL } = useUtils()
 
+const contract = import.meta.env.VITE_CONTRACT_ADDRESS
 const el = ref(window)
 const data = ref([])
 
@@ -28,8 +29,9 @@ onMounted(() => {
     <div class="text-center mt-20">
         <div v-for="(item, i) in data" :key="i" class="mt-8">
             <div class="uppercase">
-                <img :src="fixURL(item.image)" :alt="item.name" class="w-14 inline rounded-full">
-                {{ item.name }}
+                {{ i + 1 }}
+                <img :src="fixURL(item.image)" :alt="item.name" class="w-14 inline rounded-full ml-4">
+                <a :href="`https://opensea.io/assets/${contract}/${i + 1}`" target="_blank" class="underline ml-4">{{ item.name }}</a>
             </div>
             <div class="flex gap-10 justify-center text-[12px] mt-4">
                 <div v-for="(trait, i) in item.attributes" class="uppercase">
