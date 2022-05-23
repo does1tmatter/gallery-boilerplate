d99c815a7772a9cae9aeacadd960a45a1abf72d9
does1tmatter <laisvunasmsk@gmail.com>
2022-05-24 00:25:33 +0300

    bg

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -6,6 +6,6 @@ import skulls from '@/assets/img/skulls.svg'
   <div class="fixed top-0 left-0 min-h-screen min-w-full z-[-10] dark:invert transition-all opacity-[0.05]" :style="{
     backgroundImage: `url(${skulls})`
   }">
-
+  <div class="fixed top-0 left-0 min-h-screen bg-gradient-to-b from-transparent to-zinc-300 dark:to-purple-900 w-full"></div>
   </div>
 </template>
