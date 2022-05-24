897cab6256da6d1cdad886d2fe0b58d61b1ccc9f
does1tmatter <laisvunasmsk@gmail.com>
2022-05-24 05:03:17 +0300

    shadow fix

--- src/components/GalleryItems.vue
+++ src/components/GalleryItems.vue
@@ -14,8 +14,8 @@ const getImageUrl = (id) => {
 </script>
 
 <template>
-  <RouterLink v-for="(item, i) in data" :key="i" :to="`${url}${getTokenId(item.name)}`" class="relative uppercase tracking-tighter hover:scale-[1.03] transition-all rounded-xl overflow-hidden item shadow-xl shadow-black/[.15] dark:shadow-purple-200/[.15]">
-      <div :data-aos="route.name === 'collection' ? 'fade-in' : ''">
+  <RouterLink v-for="(item, i) in data" :key="i" :to="`${url}${getTokenId(item.name)}`" class="relative uppercase tracking-tighter hover:scale-[1.03] transition-all ">
+      <div :data-aos="route.name === 'collection' ? 'fade-in' : ''" class="rounded-xl overflow-hidden shadow-xl shadow-black/[.15] dark:shadow-purple-200/[.15]">
         <img :src="getImageUrl(getTokenId(item.name))" :alt="item.name">
         <div class="text-center w-full bg-zinc-600 dark:bg-purple-600 transition-all text-white text-[12px]">{{ item.name }}</div>
       </div>
